c51b010a51fb7fb8e0f7c24042e80e2e5a00c8aa
Umesh-01 <83420185+Umesh-01@users.noreply.github.com>
2021-04-29 22:25:02 +0530

    Create variables.py
    variables and strings

---
+++ variables.py
@@ -0,0 +1,13 @@
+var = 'Hello! World.'
+print(var)
+
+var1 = "Hello! World.'
+print(var1)
+
+var2 = 'this is python'
+print(var2.title())
+print(var2.upper())
+print(var2.lower())
+
+var3 = var1+ " " +var2
+print(var3)
