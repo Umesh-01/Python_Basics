8aa26ccfa2e4064297b75d0150024ba994ccc15b
Umesh-01 <83420185+Umesh-01@users.noreply.github.com>
2021-05-01 11:37:58 +0530

    Create functions.py
    functions

---
+++ functions.py
@@ -0,0 +1,21 @@
+def func():
+  print('this is the simple function.')
+
+func()
+
+
+def fun(value1, value2):
+  if value1<value2:
+    print('{value2} is greater than {value1}')
+  elif value1==value2:
+    print('Both are equal.')
+  else:
+    print('{value1} is greater than {value2}')
+
+fun(5, 10)
+fun(15, 12)
+fun(21, 21)
+
+
+# -------------------Exercise------------------
+
