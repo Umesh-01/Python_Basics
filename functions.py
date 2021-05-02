2b617ff3449d427f25cf40c91fcc6fe85d016027
Umesh-01 <83420185+Umesh-01@users.noreply.github.com>
2021-05-02 23:56:43 +0530

    Update functions.py

--- functions.py
+++ functions.py
@@ -19,3 +19,28 @@ fun(21, 21)
 
 # -------------------Exercise------------------
 
+# class in python
+
+class student():
+    def __init__(self, name, rollno):
+        self.name = name
+        self.roll = rollno
+
+    def setAge(self, age):
+        self.age = age
+
+    def setMarks(self, marks):
+        self.marks = marks
+
+    def display(self):
+        print('Student details-')
+        print('Name-', self.name)
+        print('Roll no.-', self.roll)
+        print('Age-', self.age)
+        print('Marks-', self.marks)
+
+
+stu = student('Singh', '20BCS8013')
+stu.setAge(10)
+stu.setMarks(90)
+stu.display()
