79869cb8cb177f43e2752a06ffb9974466dfb500
Umesh-01 <83420185+Umesh-01@users.noreply.github.com>
2021-05-02 23:58:31 +0530

    Create classes.py
    class with a constructor

---
+++ classes.py
@@ -0,0 +1,23 @@
+class student():
+    def __init__(self, name, rollno):
+        self.name = name
+        self.roll = rollno
+
+    def setAge(self, age):
+        self.age = age
+
+    def setMarks(self, marks):
+        self.marks = marks
+
+    def display(self):
+        print('Student details-')
+        print('Name-', self.name)
+        print('Roll no.-', self.roll)
+        print('Age-', self.age)
+        print('Marks-', self.marks)
+
+
+stu = student('Singh', '20BCS8013')
+stu.setAge(10)
+stu.setMarks(90)
+stu.display()
