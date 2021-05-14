20167db9e1efcb595f50c13b268ab0bca303d9d3
Umesh Singh <83420185+Umesh-01@users.noreply.github.com>
2021-05-14 23:54:11 +0530

    Update classes.py

--- classes.py
+++ classes.py
@@ -21,3 +21,20 @@ stu = student('Singh', '20BCS8013')
 stu.setAge(10)
 stu.setMarks(90)
 stu.display()
+
+# classes exercise
+
+# rocket with no class
+class Rocket():
+    def __init__(self):
+        self.x = 0
+        self.y = 0
+
+    def move_up(self):
+        self.y += 1
+        self.x += 1
+
+
+rocket = [Rocket() for i in range(0, 5)]
+print(rocket.y)
+print(rocket[1].move_up())
