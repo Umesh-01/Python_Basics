bce0a0f56d473b084be354300de1888a92e755d1
rounaque-h <84195978+rounaque-h@users.noreply.github.com>
2021-10-02 00:44:40 +0530

    Create String_reversal_2.py
    Python program for string reversal by recursion.

---
+++ String_reversal_2.py
@@ -0,0 +1,15 @@
+# String reversal using recursion
+  
+def reverse(strr):
+    if len(strr) == 0:
+        return strr
+    else:
+        return reverse(strr[1:]) + strr[0]
+  
+strr = "desrever_gnirtS"
+  
+print ("The original string  is : ",end="")
+print (strr)
+  
+print ("The reversed string(using recursion) is : ",end="")
+print (reverse(strr))
