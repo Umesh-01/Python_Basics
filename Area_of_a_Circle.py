0a2d1e6bb1c72c50114e70980ba1cb79af893319
Umesh Singh <83420185+Umesh-01@users.noreply.github.com>
2023-10-01 21:28:44 +0530

    Update Area_of_a_Circle.py

--- Area_of_a_Circle.py
+++ Area_of_a_Circle.py
@@ -1,11 +1,11 @@
 # Taking diameter from user
-diameter = float(input("Enter the diameter of circle : "))
+diameter = float(input("Enter the diameter of circle: "))
 
 # Taking diameter measure unit from user
-unit = input("Enter the measure unit of diameter (e.g. in, cm) : ")
+unit = input("Enter the measure unit of diameter (e.g. in, cm): ")
 
 # Finding area of a circle using ( A = 1/4 * /\ * D*D ) formula
 area = ((1 / 4) * (22 / 7) * (diameter * diameter))
 
 # Printing the area
-print("Area of circle is : ",area,unit+"2")
+print("Area of circle is: ",area,unit+"2")
